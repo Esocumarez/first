26102b49580b87476275676d552506ce878d5574
Erickson <ericksonsuarezocumarez@gmail.com>
2015-10-19 22:04:24 -0400

    First commit

---
+++ app/models/listing.rb
@@ -0,0 +1,6 @@
+class Listing < ActiveRecord::Base
+
+	 has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "not_available.jpg"
+	 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+end
